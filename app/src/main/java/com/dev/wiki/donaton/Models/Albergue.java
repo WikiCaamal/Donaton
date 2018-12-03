73ad26df037f2d6e3e7e967a0b4723e966fc1998
WilianCaamal <40349833+WilianCaamal@users.noreply.github.com>
2018-12-03 01:29:59 -0600

    Añadir modelo Albergue y Articulo

--- app/src/main/java/com/dev/wiki/donaton/Models/Albergue.java
+++ app/src/main/java/com/dev/wiki/donaton/Models/Albergue.java
@@ -1,4 +1,76 @@
 package com.dev.wiki.donaton.Models;
 
 public class Albergue {
+
+    private int id;
+    private String nombre;
+    private String direccion;
+    private String horario;
+    private String telefono;
+    private String ubicacion;
+
+    public Albergue(String nombre,String direccion, String horario, String telefono, String ubicacion){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.horario = horario;
+        this.telefono = telefono;
+        this.ubicacion = ubicacion;
+    }
+
+    public Albergue(int id,String nombre,String direccion, String horario, String telefono, String ubicacion){
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.horario = horario;
+        this.telefono = telefono;
+        this.ubicacion = ubicacion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
--- app/src/main/java/com/dev/wiki/donaton/Models/Articulo.java
+++ app/src/main/java/com/dev/wiki/donaton/Models/Articulo.java
@@ -1,7 +1,85 @@
 package com.dev.wiki.donaton.Models;
 
-import android.location.Location;
-
 public class Articulo {
-    private String nombre, descripcion, direccion;
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String direccion;
+    private String dimenciones;
+    private String estado;
+    private String categoria;
+
+    public Articulo(Integer id, String nombre, String descripcion, String direccion, String dimenciones, String estado, String categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.direccion = direccion;
+        this.dimenciones = dimenciones;
+        this.estado = estado;
+        this.categoria = categoria;
+    }
+    public Articulo(String nombre, String descripcion, String direccion, String dimenciones, String estado, String categoria) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.direccion = direccion;
+        this.dimenciones = dimenciones;
+        this.estado = estado;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDimenciones() {
+        return dimenciones;
+    }
+
+    public void setDimenciones(String dimenciones) {
+        this.dimenciones = dimenciones;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
