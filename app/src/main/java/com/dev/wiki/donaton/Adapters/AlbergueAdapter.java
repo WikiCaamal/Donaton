9488489957e5e5636a57fa6c0397f4b48d1f8650
WilianCaamal <40349833+WilianCaamal@users.noreply.github.com>
2018-12-03 11:35:12 -0600

    Datos de prueba en lista de Albergues

--- app/src/main/java/com/dev/wiki/donaton/Adapters/AlbergueAdapter.java
+++ app/src/main/java/com/dev/wiki/donaton/Adapters/AlbergueAdapter.java
@@ -1,4 +1,59 @@
 package com.dev.wiki.donaton.Adapters;
 
-public class AlbergueAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dev.wiki.donaton.Models.Albergue;
+import com.dev.wiki.donaton.R;
+
+import java.util.ArrayList;
+
+public class AlbergueAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<Albergue> listAlbergues = new ArrayList<>();
+
+    public AlbergueAdapter(Context context, ArrayList<Albergue> listAlbergues) {
+        this.context = context;
+        this.listAlbergues = listAlbergues;
+    }
+
+    @Override
+    public int getCount() {
+        return listAlbergues.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return listAlbergues.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Albergue albergue = (Albergue) getItem(i);
+
+        view = LayoutInflater.from(context).inflate(R.layout.item_albergue,null);
+        ImageView img = view.findViewById(R.id.img_item);
+        TextView nombre = view.findViewById(R.id.nombre);
+        TextView direccion = view.findViewById(R.id.direccion);
+        TextView ubicacion = view.findViewById(R.id.ubicacion);
+
+        //TODO: Agregar la carga de imagen
+
+        nombre.setText(albergue.getNombre());
+        direccion.setText(albergue.getDireccion());
+        ubicacion.setText(albergue.getUbicacion());
+
+        return view;
+    }
 }
--- app/src/main/java/com/dev/wiki/donaton/AlberguesActivity.java
+++ app/src/main/java/com/dev/wiki/donaton/AlberguesActivity.java
@@ -2,12 +2,44 @@ package com.dev.wiki.donaton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.dev.wiki.donaton.Adapters.AlbergueAdapter;
+import com.dev.wiki.donaton.Models.Albergue;
+
+import java.util.ArrayList;
 
 public class AlberguesActivity extends AppCompatActivity {
+    private ListView listAlbergue;
+    private Albergue albergue;
+    private ArrayList<Albergue> listAlbergues;
+
+    private void initComponents(){
+        listAlbergue = findViewById(R.id.list);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albergues);
+        initComponents();
+
+        AlbergueAdapter adapter = new AlbergueAdapter(this,testData());
+        listAlbergue.setAdapter(adapter);
     }
+
+    //TODO: Eliminar despues de las pruebas
+    public ArrayList<Albergue> testData(){
+        listAlbergues = new ArrayList<>();
+        listAlbergues.add(new Albergue( "Albegue 1","C.00 x 00 y 00", "9:00 am - 2:00 pm", "999-369-8524", "Tekax"));
+        listAlbergues.add(new Albergue( "Albegue 2","C.00 x 00 y 00", "8:00 am - 4:00 pm", "999-369-8524", "Oxkutzcab"));
+        listAlbergues.add(new Albergue( "Albegue 3","C.00 x 00 y 00", "7:00 am - 5:00 pm", "999-369-8524", "Merida"));
+        listAlbergues.add(new Albergue( "Albegue 4","C.00 x 00 y 00", "9:30 am - 7:00 pm", "999-369-8524", "Uman"));
+        listAlbergues.add(new Albergue( "Albegue 5","C.00 x 00 y 00", "10:00 am - 10:00 pm", "999-369-8524", "Akil"));
+        return listAlbergues;
+    }
+
+
 }
