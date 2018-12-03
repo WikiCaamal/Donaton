d1a142f6ce999883f08c301ccb4ff9d78d17df3c
WilianCaamal <40349833+WilianCaamal@users.noreply.github.com>
2018-12-03 01:26:44 -0600

    Mejorando la estructura de la clase

--- app/src/main/java/com/dev/wiki/donaton/Dao/DaoPerson.java
+++ app/src/main/java/com/dev/wiki/donaton/Dao/DaoPerson.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.dev.wiki.donaton.DataBase.BaseHelper;
+import com.dev.wiki.donaton.DataBase.Personas;
 import com.dev.wiki.donaton.Models.Person;
 
 import java.util.ArrayList;
@@ -15,39 +16,34 @@ public class DaoPerson {
     private BaseHelper baseHelper;
     private SQLiteDatabase dbWrite;
     private SQLiteDatabase dbRead;
-    private Context context;
     private ContentValues registro;
-    private String table = "Personas";
     private Person person;
 
     public DaoPerson(Context context){
-        this.context = context;
         this.baseHelper = new BaseHelper(context,"Donaton.db",null,1);
-      this.dbWrite = this.baseHelper.getWritableDatabase();
+        this.dbWrite = this.baseHelper.getWritableDatabase();
         this.dbRead = this.baseHelper.getReadableDatabase();
     }
 
-    private ArrayList<Person> listPersons = new ArrayList<>();
+    private ArrayList<Person> listPersons;
 
     public ArrayList<Person> getAll(){
+        listPersons = new ArrayList<>();
         try {
-
             if (dbRead!=null){
-                Cursor persons = dbRead.rawQuery("Select * from Personas",null);
-                int count = persons.getCount();
-                int index = 0;
-                String[] arreglo = new String[count];
-                if (persons.moveToFirst()){
+                Cursor c = dbRead.rawQuery("Select * from Personas",null);
+                if (c.moveToFirst()){
                     do{
                         person = new Person(
-                                persons.getInt(0),
-                                persons.getString(1),
-                                persons.getString(2),
-                                persons.getString(3),
-                                persons.getString(4),
-                                persons.getString(5)
+                            c.getInt(c.getColumnIndex(Personas.COLUMN_ID)),
+                            c.getString(c.getColumnIndex(Personas.COLUMN_NOMBRE)),
+                            c.getString(c.getColumnIndex(Personas.COLUMN_APELLIDO)),
+                            c.getString(c.getColumnIndex(Personas.COLUMN_CORREO)),
+                            c.getString(c.getColumnIndex(Personas.COLUMN_USUARIO)),
+                            c.getString(c.getColumnIndex(Personas.COLUMN_CONTRASENA))
                         );
-                    }while (persons.moveToNext());
+                        listPersons.add(person);
+                    }while (c.moveToNext());
                 }
                 return  listPersons;
             }else {return null;}
@@ -56,17 +52,20 @@ public class DaoPerson {
         }
     }
 
+    public ContentValues getValuesRegistro(Person person){
+        registro = new ContentValues();
+        registro.put(Personas.COLUMN_NOMBRE,    person.getName());
+        registro.put(Personas.COLUMN_APELLIDO,  person.getLastName());
+        registro.put(Personas.COLUMN_CORREO,    person.getEmail());
+        registro.put(Personas.COLUMN_USUARIO,   person.getUser());
+        registro.put(Personas.COLUMN_CONTRASENA,person.getPassword());
+        return registro;
+    }
+
     public boolean insert(Person person){
         try {
             if (dbWrite!=null){
-                registro = new ContentValues();
-                registro.put("Nombre",person.getName());
-                registro.put("Apellido",person.getLastName());
-                registro.put("Usuario",person.getUser());
-                registro.put("Contrasena",person.getPassword());
-
-                long result = dbWrite.insert(table,null,registro);
-                if (result != 0){
+                if (dbWrite.insert(Personas.TABLE_NAME,null,getValuesRegistro(person)) != -1){
                     return true;
                 }
                 else {
@@ -74,16 +73,48 @@ public class DaoPerson {
                 }
             }else {return false;}
         }catch (Exception e){
+            Log.e("SQLite_Error",e.getMessage());
             return false;
         }
     }
 
     public boolean update(Person person){
-        return true;
+
+        try {
+            if (dbWrite != null){
+                String condicion = Personas.COLUMN_ID + "= ?";
+                String[] args = new String[]{person.getId().toString()};
+                if (dbWrite.update(Personas.TABLE_NAME,getValuesRegistro(person),condicion,args) != -1){
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
     }
 
     public boolean delete(Integer id){
-        return true;
+        try {
+            if (dbWrite != null){
+                String condicion = Personas.COLUMN_ID + " = ?";
+                String[] args = new String[]{id.toString()};
+                if (dbWrite.delete(Personas.TABLE_NAME,condicion,args) != 0){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }else {
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
     }
 
 }
